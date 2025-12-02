040b9e842f4d82bc3d1f303e921c866899649457
Luke Deffenbaugh <lukedeffenbaugh@gmail.com>
2025-12-02 14:34:06 -0600

    Completed both challenges for day 2

---
+++ challenges/chal02.cpp
@@ -0,0 +1,90 @@
+#include <chal02.h>
+
+size_t advent::challenge_entry(
+        const vector<string>& data,
+        shared_ptr<string[]> buffer,
+        const string& input
+    )
+{
+    // Setting up regex
+    regex re(R"(((\d+)-(\d+)))");
+    smatch match;
+
+    // Move puzzle input into a non-const string (this puzzle is a one-liner)
+    string puzzle_input = data[0];
+    // Create puzzle outputs
+    uint_fast64_t part1 = 0;
+    uint_fast64_t part2 = 0;
+
+    // Loop over puzzle input using regex match.
+    while (regex_search(puzzle_input, match, re))
+    {
+        // Grab the min and max of range for readability
+        uint_fast64_t min = stoull(match[2]);
+        uint_fast64_t max = stoull(match[3]);
+        cout << min << " ";
+        for (uint_fast64_t i = min; i <= max; i++)
+        {
+            // Check part 1
+            if (check_invalid_1(to_string(i)))
+            {
+                // ID is invalid
+                part1 += i;
+            }
+
+            // Check part 2
+            if (check_invalid_2(to_string(i)))
+            {
+                // ID is invalid
+                part2 += i;
+            }
+
+        }
+        cout << max << endl;
+
+        // Advance the puzzle input
+        puzzle_input = match.suffix();
+    }
+
+    buffer.get()[0] = to_string(part1);
+    buffer.get()[1] = to_string(part2);
+
+    return 0;
+}
+
+bool advent::check_invalid_1(const string id)
+{
+    // Skip Anything non-even in length
+    if (id.length() % 2 != 0) return false;
+
+    // Grab first half of string and right half
+    string lh = id.substr(0, id.length()/2);
+    string rh = id.substr(id.length()/2);
+
+    // Return if both halves are equal
+    return lh == rh;
+}
+
+bool advent::check_invalid_2(const string id)
+{
+    for (int i = 1; i <= id.length()/2; i++)
+    {
+        // declair a test ID
+        string tst_id;
+
+        // Skip non-divisible i's
+        if (id.length() % i != 0) continue;
+
+        // Grab pattern
+        string pat = id.substr(0, i);
+
+        // Create full repeating pat that is equivalent to length of id
+        for (int j = 0; j < id.length() / i; j++)
+        {
+            tst_id = tst_id + pat;
+        }
+
+        if (tst_id == id) return true;
+    }
+    return false;
+}
---
+++ include/chal02.h
@@ -0,0 +1,37 @@
+#ifndef AOC25_CHALLENGE
+#define AOC25_CHALLENGE
+
+#include <string>
+#include <iostream>
+#include <memory>
+#include <vector>
+#include <regex>
+#include <format>
+/*
+ * Extra includes here
+ */
+
+using namespace std;
+
+namespace advent
+{
+    /**
+     * Standard challenge code entrypoint
+     * @param data string file data to be read
+     * @param buffer string pointer buffer for output
+     * @param input optional string input
+     * @return size_t status (zero on success, non-zero on fail)
+     */
+    size_t challenge_entry(
+        const vector<string>& data,
+        shared_ptr<string[]> buffer,
+        const string& input
+    );
+
+    // Handlers for part 1 and 2
+    bool check_invalid_1(string id);
+    bool check_invalid_2(string id);
+
+}
+
+#endif //AOC25_CHALLENGE
